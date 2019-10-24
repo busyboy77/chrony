692fa457ee9043a1726c1198194917ef34b6c58c
Nasir Mahmood <nasir.mahmood@gmail.com>
2019-10-24 19:21:48 +0500

    Add Mofile manifest and config file

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class chrony {
+  ensure_packages('[chrony]')
+
+  file {'/etc/chrony.conf':
+    source  => 'puppet://modules/chrony/chrony.conf',
+    notify  => Service['chronyd'],
+    require => Package['chrony'],
+       }
+
+  service {'chronyd':
+    ensure => running,
+    enable => true,
+  }
+}
